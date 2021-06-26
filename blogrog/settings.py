385ad4d6dca0f2bf4abd07f8f128a3f513cd90ba
rogfab <rogfab1510@gmail.com>
2021-06-26 19:58:20 +1100

    enlever la base de données par défaut postgresql dans le fichier settings.txt

--- blogrog/settings.py
+++ blogrog/settings.py
@@ -79,18 +79,18 @@ WSGI_APPLICATION = 'blogrog.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blogrog',
-        'HOST': 'localhost',
-        'USER': 'blogrogdbadmin',
-        'PASSWORD': '6yAZK68CzS!!wNF',
-        'PORT': '5431'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'blogrog',
+    #     'HOST': 'localhost',
+    #     'USER': 'blogrogdbadmin',
+    #     'PASSWORD': '6yAZK68CzS!!wNF',
+    #     'PORT': '5431'
+    # }
 }
 
 
